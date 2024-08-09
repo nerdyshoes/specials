8b05fff471fc40dfa67b46b6575e6dd6d23a4e70
Benjamin Mason <u7474632@anu.edu.au>
2024-08-09 14:19:16 +1000

    new things

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import requests
 import random
 from bs4 import BeautifulSoup
-
+import pandas as pd
+import time
 
 UAS = ("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1", 
        "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
@@ -30,9 +31,25 @@ def get_price(url):
        
        if len(soup_price) == 0:
               print("failed")
-              with open("output1.html", "w") as file:
-                     file.write(str(soup))
+              return 0
+
+
        else:
               price = soup_price[0].text
               raw_price = price.replace("$", "")
               print(raw_price)
+
+              return raw_price
+
+
+
+df = pd.read_csv('shopping_list.csv')
+
+links = df["Link"].to_list()
+for n, link in enumerate(links):
+       price = get_price(link)
+       df.at[n, "Price"] = price
+       time.sleep(3 + random.randrange(-2, 2))
+
+print(df)
+df.to_csv('shopping_list.csv', index=False)
